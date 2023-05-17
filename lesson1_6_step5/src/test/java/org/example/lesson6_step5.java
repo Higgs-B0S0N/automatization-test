0b1fa76b50d963cf7d9c3f39f5fd11eee198b6c6
Higgs-B0S0N <fv4202pt@gmail.com>
2023-05-17 13:02:57 +0300

    add code

--- lesson1_6_step5/src/test/java/org/example/lesson6_step5.java
+++ lesson1_6_step5/src/test/java/org/example/lesson6_step5.java
@@ -1,4 +1,44 @@
 package org.example;
 
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class lesson6_step5 {
+    WebDriver driver = new ChromeDriver();
+
+        @Before
+        public void beforeTest(){
+            driver.get("http://suninjuly.github.io/find_link_text");
+        }
+        @Test
+        public void Test(){
+            int a = (int)Math.ceil(Math.pow(Math.PI, Math.E)*10000);
+            String stringValue = Integer.toString(a);
+
+            WebElement link = driver.findElement(new By.ByLinkText(stringValue));
+            link.click();
+            WebElement input1 = driver.findElement(new By.ByTagName("input"));
+            input1.click();
+            input1.sendKeys("Ivan");
+            WebElement input2 = driver.findElement(new By.ByName("last_name"));
+            input2.click();
+            input2.sendKeys("Petrov");
+            WebElement input3 = driver.findElement(new By.ByClassName("city"));
+            input3.click();
+            input3.sendKeys("Smolensk");
+            WebElement input4 = driver.findElement(new By.ById("country"));
+            input4.click();
+            input4.sendKeys("Russia");
+            WebElement submitBtn = driver.findElement(new By.ByCssSelector("button.btn"));
+            submitBtn.click();
+        }
+        @After
+        public void afterTest(){
+            driver.quit();
+        }
 }
