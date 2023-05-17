e7de3f933e08f7949a8b4d59c027eaba6e986d05
Higgs-B0S0N <fv4202pt@gmail.com>
2023-05-17 10:40:23 +0300

    folder structure introduced

--- lesson1.6_step4/src/test/java/org/example/lesson6_step4.java
+++ lesson1.6_step4/src/test/java/org/example/lesson6_step4.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,33 +9,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class lesson6_step4 {
     WebDriver driver = new ChromeDriver();
-    @Before
-    public void beforeTest(){
-        driver.get("http://suninjuly.github.io/simple_form_find_task.html");
+
+    public lesson6_step4() {
     }
+
+    @Before
+    public void beforeTest() {
+        this.driver.get("http://suninjuly.github.io/simple_form_find_task.html");
+    }
+
     @Test
-    public void test(){
-        WebElement webElement1 = driver.findElement(new By.ByTagName("input"));
+    public void test() {
+        WebElement webElement1 = this.driver.findElement(new By.ByTagName("input"));
         webElement1.click();
-        webElement1.sendKeys("Ivan");
-
-        WebElement webElement2 = driver.findElement(new By.ByName("last_name"));
+        webElement1.sendKeys(new CharSequence[]{"Ivan"});
+        WebElement webElement2 = this.driver.findElement(new By.ByName("last_name"));
         webElement2.click();
-        webElement2.sendKeys("Petrov");
-
-        WebElement webElement3 = driver.findElement(new By.ByClassName("city"));
+        webElement2.sendKeys(new CharSequence[]{"Petrov"});
+        WebElement webElement3 = this.driver.findElement(new By.ByClassName("city"));
         webElement3.click();
-        webElement3.sendKeys("Smolensk");
-
-        WebElement webElement4 = driver.findElement(new By.ById("country"));
+        webElement3.sendKeys(new CharSequence[]{"Smolensk"});
+        WebElement webElement4 = this.driver.findElement(new By.ById("country"));
         webElement4.click();
-        webElement4.sendKeys("Russia");
-
-        WebElement submitBtn = driver.findElement(new By.ByCssSelector("button.btn"));
+        webElement4.sendKeys(new CharSequence[]{"Russia"});
+        WebElement submitBtn = this.driver.findElement(new By.ByCssSelector("button.btn"));
         submitBtn.click();
     }
+
     @After
-    public void afterTest(){
-        driver.quit();
+    public void afterTest() {
+        this.driver.quit();
     }
 }
